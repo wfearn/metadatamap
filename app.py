78a8765cad09463426964a6f6baca61dc64986ba
Alison Smith <alison.smith.m@gmail.com>
2019-07-22 16:38:09 -0400

    increment training data

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ PRIOR_ATTR = 'lambda' # UNUSED
 
 # Number of unlabled docs to show per iteration on the web
 #UNLABELED_COUNT = 10
-UNLABELED_COUNT = 100
+UNLABELED_COUNT = 200
 
 # Seed used in the shuffle
 SHUFFLE_SEED = None #8448
@@ -57,7 +57,7 @@ USER_LABEL_ATTR = 'user_label'
 #  the pickle, generating a new pickle if one of that name doesn't already
 #  exist)
 NUM_TOPICS = 10
-PRELABELED_SIZE = 1857
+PRELABELED_SIZE = 50
 LABEL_WEIGHT = 1
 USER_ID_LENGTH = 5
 
