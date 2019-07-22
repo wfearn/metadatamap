beb08b3f35c7fff45486bbb37f9feac20f0d797b
Alison Smith <alison.smith.m@gmail.com>
2019-07-22 16:25:43 -0400

    turn on cheat

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ USER_LABEL_ATTR = 'user_label'
 #  the pickle, generating a new pickle if one of that name doesn't already
 #  exist)
 NUM_TOPICS = 10
-PRELABELED_SIZE = 500
+PRELABELED_SIZE = 1000
 LABEL_WEIGHT = 1
 USER_ID_LENGTH = 5
 
